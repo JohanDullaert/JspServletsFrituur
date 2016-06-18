4812e6b4b6f40a398795b65ea8416dfacfc8bfd7
Johan Dullaert <johandullaert@telenet.be>
2016-06-18 10:53:26 +0200

    up

--- JspServletsFrituurFrida/src/be/vdab/repositories/SausRepository.java
+++ JspServletsFrituurFrida/src/be/vdab/repositories/SausRepository.java
@@ -1,14 +1,30 @@
 package be.vdab.repositories;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import be.vdab.entities.Saus;
 
 public class SausRepository {
 	
-	private final static Map<Long, Saus> SAUZEN = new HashMap<>();
+	// oef 10 MVC: er is nog geen sprake van toevoegen of verwijderen van suazen, 
+	//dus concurrent lijkt me overbodig op deze moment (cursus is wel met create)
+	private final static Map<Long, Saus> SAUZEN = new ConcurrentHashMap<>();
 	
+	static {
+		SAUZEN.put(1L, new Saus(1, "mayonaise", new String[]{"ei", "olie", "mosterd"}));
+		SAUZEN.put(2L, new Saus(2, "cocktail", new String[]{}));
+		SAUZEN.put(3L, new Saus(3, "mosterd", new String[]{"chilli"}));
+		SAUZEN.put(4L, new Saus(4, "tartare", new String[]{"mayo", "pikkels"}));
+		SAUZEN.put(5L, new Saus(5, "vinaigrette", new String[]{"olie", "honing", "bieslook"}));
+	}
 	
+	// waarom niet ineens een list van sauzen maken ipv een map? (cfr cursus)
+	// Is dat om een databank wat te faken?
+	public List<Saus> findAll() {
+		return new ArrayList<>(SAUZEN.values());
+	}
 
 }
--- JspServletsFrituurFrida/src/be/vdab/servlets/SausServlet.java
+++ JspServletsFrituurFrida/src/be/vdab/servlets/SausServlet.java
@@ -1,8 +1,6 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import be.vdab.entities.Saus;
+import be.vdab.repositories.SausRepository;
 
 /**
  * Servlet implementation class SausServlet
@@ -20,23 +18,26 @@ public class SausServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/JSP/sauzen.jsp";
+	private final SausRepository sausRepository = new SausRepository();
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Saus> sauzen = new ArrayList<>();
-		try {
-			sauzen.add(new Saus(1, "mayonaise", new String[]{"ei", "olie", "mosterd"}));
-			sauzen.add(new Saus(2, "cocktail", new String[]{}));
-			sauzen.add(new Saus(2, "mosterd", new String[]{"chilli"}));
-			sauzen.add(new Saus(2, "tartare", new String[]{"mayo", "pikkels"}));
-			sauzen.add(new Saus(2, "vinaigrette", new String[]{"olie", "honing", "bieslook"}));
-		} catch (Exception e) {
-			// ignoring exception: I'm responsible for correct instances here...
-			e.printStackTrace();
-		}
-		request.setAttribute("sauzen", sauzen);
+		
+		// na 10 MVC niet meer nodig, nu via DAO (ofte Repository)
+//		List<Saus> sauzen = new ArrayList<>();
+//		try {
+//			sauzen.add(new Saus(1, "mayonaise", new String[]{"ei", "olie", "mosterd"}));
+//			sauzen.add(new Saus(2, "cocktail", new String[]{}));
+//			sauzen.add(new Saus(3, "mosterd", new String[]{"chilli"}));
+//			sauzen.add(new Saus(4, "tartare", new String[]{"mayo", "pikkels"}));
+//			sauzen.add(new Saus(5, "vinaigrette", new String[]{"olie", "honing", "bieslook"}));
+//		} catch (Exception e) {
+//			// ignoring exception: I'm responsible for correct instances here...
+//			e.printStackTrace();
+//		}			 
+		request.setAttribute("sauzen", sausRepository.findAll());
 		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 
