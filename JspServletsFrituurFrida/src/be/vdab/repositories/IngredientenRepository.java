620f2ca6cdf05829335057bfbdc3fc4621971b30
Johan Dullaert <johandullaert@telenet.be>
2016-07-05 06:08:17 +0200

    a bit of cleaning

--- JspServletsFrituurFrida/src/be/vdab/repositories/IngredientenRepository.java
+++ JspServletsFrituurFrida/src/be/vdab/repositories/IngredientenRepository.java
@@ -4,21 +4,17 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import be.vdab.entities.Saus;	
+import be.vdab.entities.Saus;
 
 public class IngredientenRepository {
-	
+
 	private final SausRepository sausRepository = new SausRepository();
-	
-	public Set<String> findAll(){
+
+	public Set<String> findAll() {
 		Set<String> ingredienten = new HashSet<>();
 		for (Saus saus : sausRepository.findAll()) {
 			ingredienten.addAll(Arrays.asList(saus.getIngredienten()));
 		}
 		return ingredienten;
 	}
-	
-	
-	
-
 }
